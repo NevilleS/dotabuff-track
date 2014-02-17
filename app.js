a058fd7d0a657c192d71c77a359afbff53a6f282
Neville Samuell <nevilles@gmail.com>
2014-02-17 18:55:11 -0500

    Change template engine to ejs-locals, refactor routes

--- app.js
+++ app.js
@@ -4,19 +4,19 @@
  */
 
 var express = require('express');
+var engine = require('ejs-locals');
 var routes = require('./routes');
+var track = require('./routes/track');
 var http = require('http');
 var path = require('path');
 var sass = require('node-sass');
-var request = require('request');
-var cheerio = require('cheerio');
-var async = require('async');
 
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
+app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
@@ -37,166 +37,7 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/track', function(req, res) {
-    var url = req.query.url;
-    console.log("Received update url: " + url);
-    var data = {
-        heroesPlayed: [],
-        heroesRemaining: [],
-        heroesNoWins: []
-    };
-
-    // request dotabuff data
-    var dotabuffMatch = /^(?:http:\/\/)?dotabuff\.com\/players\/(\d+)$/.exec(url);
-    try {
-        if (dotabuffMatch && dotabuffMatch[1]) {
-            var playerId = dotabuffMatch[1];
-            console.log("Parsed dotabuff url, playerId: " + playerId);
-
-            // setup two async requests in parallel
-            var asyncTasks = [];
-            asyncTasks.push(function(callback) {
-                // Get the players hero page
-                var options = {
-                    url: "http://dotabuff.com/players/" + playerId + "/heroes",
-                    headers: {
-                        'User-Agent': 'request'
-                    }
-                };
-                request(options, function(err, resp, body) {
-                    if (err) {
-                        console.log("request err: " + err);
-                        return;
-                    }
-
-                    // scrape the body for heroes played by the playerId
-                    $ = cheerio.load(body);
-                    var titleText = $("#content-header .content-header-title h1").text();
-                    var nameMatch = /(.*?)Heroes/.exec(titleText);
-                    if (nameMatch && nameMatch[1]) {
-                        data.playerName = nameMatch[1];
-                        console.log("playerName: " + data.playerName);
-                    }
-                    data.heroesPlayed = [];
-                    $("article tbody tr").each(function() {
-                        var heroName, heroImg, heroGames, heroWinrate, heroKDA = undefined;
-
-                        // There are three 
-                        var tdNum = 0;
-                        $(this).children().each(function() {
-                            switch(tdNum) {
-                                case 0: // heroImg
-                                    heroImg = $(this).find(".image-hero").attr('src');
-                                    break;
-                                case 1: // heroName
-                                    heroName = $(this).find(".hero-link").text();
-                                    break;
-                                case 2: // heroGames
-                                    heroGames = parseInt($(this).children().first().text());
-                                    break;
-                                case 3: // heroWinrate
-                                    heroWinrate = parseFloat($(this).children().first().text());
-                                    break;
-                                case 4: // heroKDA
-                                    heroKDA = parseFloat($(this).children().first().text());
-                                    break;
-                                default:
-                            }
-                            tdNum += 1;
-                        });
-
-                        // Push results into heroesPlayed array
-                        console.log([heroName, heroGames, heroWinrate, heroKDA].join(", "));
-                        if (heroName !== undefined && heroImg !== undefined && heroGames !== undefined &&
-                                heroWinrate !== undefined && heroKDA !== undefined) {
-                            console.log("Added " + heroName);
-                            var hero = {name: heroName, image: heroImg, games: heroGames, winrate: heroWinrate, KDA: heroKDA};
-                            data.heroesPlayed.push(hero);
-                        }
-                    });
-                    console.log("heroesPlayed: " + data.heroesPlayed.length);
-                    callback();
-                });
-            });
-            asyncTasks.push(function(callback) {
-                // Get the players hero page
-                var options = {
-                    url: "http://dotabuff.com/heroes/played",
-                    headers: {
-                        'User-Agent': 'request'
-                    }
-                };
-                request(options, function(err, resp, body) {
-                    if (err) {
-                        console.log("request err: " + err);
-                        return;
-                    }
-
-                    // scrape the body for all heroes
-                    var heroesAvailable = [];
-                    $ = cheerio.load(body);
-                    data.heroesAvailable = [];
-                    $("article tbody tr").each(function() {
-                        var heroName = $(this).find(".hero-link").text();
-                        var heroImg = $(this).find(".image-hero").attr('src');
-                        if (heroName && heroImg) {
-                            data.heroesAvailable.push({name: heroName, image: heroImg});
-                        }
-                    });
-                    console.log("heroesAvailable: " + data.heroesAvailable.length);
-                    callback();
-                });
-            });
-            async.parallel(asyncTasks, function() {
-                console.log("Both callbacks finished, find heroes remaining and no wins");
-                var heroesRemaining = [];
-                var heroesNoWins = [];
-                for (i = 0; i < data.heroesAvailable.length; i++) {
-                    var name = data.heroesAvailable[i].name;
-                    var found = false;
-                    var wins = false;
-                    for (j = 0; !found && j < data.heroesPlayed.length; j++) {
-                        if (data.heroesPlayed[j].name === name) {
-                            //console.log("Found " + name + " in heroesPlayed");
-                            found = true;
-                            if (data.heroesPlayed[j].winrate > 0) {
-                                wins = true;
-                            }
-                        }
-                    }
-                    if (!found) {
-                        //console.log("Didn't find " + name + " in heroesPlayed");
-                        heroesRemaining.push(data.heroesAvailable[i]);
-                        heroesNoWins.push(data.heroesAvailable[i]);
-                    } else if (!wins) {
-                        heroesNoWins.push(data.heroesAvailable[i]);
-                        //console.log("Found " + name + " in heroesPlayed, but no wins");
-                    }
-                }
-                data.heroesRemaining = heroesRemaining;
-                data.heroesNoWins = heroesNoWins;
-                console.log("heroesRemaining: " + data.heroesRemaining.length);
-                console.log("heroesNoWins: " + data.heroesNoWins.length);
-
-                // Send response
-                res.send(data);
-            });
-        } else {
-            throw "Bad Dotabuff URL, must link to a player page (e.g. http://dotabuff.com/players/109473826)";
-        }
-    } catch (err) {
-        console.log("caught err");
-        if (err && err.message) {
-            data.err = err.message;
-        } else if (err) {
-            data.err = err;
-        } else {
-            data.err = "Unknown error";
-        }
-        res.send(data);
-        return;
-    }
-});
+app.get('/track', track.track);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,84 +1,84 @@
-$(document).ready(function() {
-    // Update the tracker data
-    var updateTracker = function(data) {
-        if (data && data.err) {
-            alert(data.err);
-            return;
-        }
-        if (data && data.playerName &&
-                data.heroesPlayed && data.heroesPlayed.length !== undefined &&
-                data.heroesRemaining && data.heroesRemaining.length !== undefined &&
-                data.heroesNoWins && data.heroesNoWins.length !== undefined) {
-            $("#playerName").html(data.playerName);        
-
-            // Render hero plays data
-            var numHeroesPlayed = data.heroesPlayed.length;
-            var numHeroesRemaining = data.heroesRemaining.length;
-            var numHeroesTotal = numHeroesPlayed + data.heroesRemaining.length;
-            var heroPlayedPercentage = Math.round((numHeroesPlayed / numHeroesTotal) * 100);
-            $("#heroesPlayedText").html(numHeroesPlayed + "/" + numHeroesTotal + " (" + heroPlayedPercentage + "%)");
-            var heroesPlayedChartData = [
-                {value: numHeroesPlayed, color: "#268bd2"},
-                {value: numHeroesRemaining, color: "#93a1a1"}
-            ];
-            var ctx = $("#heroesPlayedChart").get(0).getContext("2d");
-            var heroesPlayedChart = new Chart(ctx).Doughnut(heroesPlayedChartData);
-
-            // Update heroes remaining
-            $("#heroesRemainingText").html(numHeroesRemaining);
-            if (numHeroesRemaining > 0) {
-                // Wipe table and repopulate with data.heroesRemaining
-                var heroesRemainingTable = $("#heroesRemainingTable");
-                heroesRemainingTable.empty();
-                for (i = 0; i < data.heroesRemaining.length; i++) {
-                    console.log("data.heroesRemaining[" + i + "]: " + data.heroesRemaining[i]);
-                    var heroName = data.heroesRemaining[i].name;
-                    var heroImage = data.heroesRemaining[i].image;
-                    console.log("add " + heroName);
-                    heroesRemainingTable.append("<tr><td class=\"heroText\"><p>" + heroName + "</p><td class=\"heroImage\"><img src=\"" + heroImage + "\" /></td></tr>");
-                }
-            } else {
-                $("#heroesRemaining").fadeOut(3000);
-            }
-
-            // Render hero wins chart data
-            var numHeroesNoWins = data.heroesNoWins.length;
-            var numHeroesWon = numHeroesTotal - numHeroesNoWins;
-            var heroesWonPercentage = Math.round((numHeroesWon / numHeroesTotal) * 100);
-            $("#heroesWonText").html(numHeroesWon + "/" + numHeroesTotal + " (" + heroesWonPercentage + "%)");
-            var heroesWonChartData = [
-                {value: numHeroesWon, color: "#268bd2"},
-                {value: numHeroesNoWins, color: "#93a1a1"}
-            ];
-            var ctx = $("#heroesWonChart").get(0).getContext("2d");
-            var heroesWonChart = new Chart(ctx).Doughnut(heroesWonChartData);
-
-            // Update heroes no wins 
-            $("#heroesNoWinsText").html(numHeroesNoWins);
-            if (numHeroesNoWins > 0) {
-                // Wipe table and repopulate with data.heroesNoWins
-                var heroesNoWinsTable = $("#heroesNoWinsTable");
-                heroesNoWinsTable.empty();
-                for (i = 0; i < data.heroesNoWins.length; i++) {
-                    console.log("data.heroesNoWins[" + i + "]: " + data.heroesNoWins[i]);
-                    var heroName = data.heroesNoWins[i].name;
-                    var heroImage = data.heroesNoWins[i].image;
-                    console.log("add " + heroName);
-                    heroesNoWinsTable.append("<tr><td class=\"heroText\"><p>" + heroName + "</p><td class=\"heroImage\"><img src=\"" + heroImage + "\" /></td></tr>");
-                }
-            } else {
-                $("#heroesNoWins").fadeOut(3000);
-            }
-
-            // Show the tracker content
-            $("#introContent").hide();
-            $("#trackerContent").show();
-        } else {
-            $("#introContent").show();
-            $("#trackerContent").hide();
-        }
+// Update the tracker data
+var updateTracker = function(data) {
+    if (data && data.err) {
+        alert(data.err);
+        return;
     }
+    if (data && data.playerName &&
+            data.heroesPlayed && data.heroesPlayed.length !== undefined &&
+            data.heroesRemaining && data.heroesRemaining.length !== undefined &&
+            data.heroesNoWins && data.heroesNoWins.length !== undefined) {
+        $("#playerName").html(data.playerName);        
 
+        // Render hero plays data
+        var numHeroesPlayed = data.heroesPlayed.length;
+        var numHeroesRemaining = data.heroesRemaining.length;
+        var numHeroesTotal = numHeroesPlayed + data.heroesRemaining.length;
+        var heroPlayedPercentage = Math.round((numHeroesPlayed / numHeroesTotal) * 100);
+        $("#heroesPlayedText").html(numHeroesPlayed + "/" + numHeroesTotal + " (" + heroPlayedPercentage + "%)");
+        var heroesPlayedChartData = [
+            {value: numHeroesPlayed, color: "#268bd2"},
+            {value: numHeroesRemaining, color: "#93a1a1"}
+        ];
+        var ctx = $("#heroesPlayedChart").get(0).getContext("2d");
+        var heroesPlayedChart = new Chart(ctx).Doughnut(heroesPlayedChartData);
+
+        // Update heroes remaining
+        $("#heroesRemainingText").html(numHeroesRemaining);
+        if (numHeroesRemaining > 0) {
+            // Wipe table and repopulate with data.heroesRemaining
+            var heroesRemainingTable = $("#heroesRemainingTable");
+            heroesRemainingTable.empty();
+            for (i = 0; i < data.heroesRemaining.length; i++) {
+                console.log("data.heroesRemaining[" + i + "]: " + data.heroesRemaining[i]);
+                var heroName = data.heroesRemaining[i].name;
+                var heroImage = data.heroesRemaining[i].image;
+                console.log("add " + heroName);
+                heroesRemainingTable.append("<tr><td class=\"heroText\"><p>" + heroName + "</p><td class=\"heroImage\"><img src=\"" + heroImage + "\" /></td></tr>");
+            }
+        } else {
+            $("#heroesRemaining").fadeOut(3000);
+        }
+
+        // Render hero wins chart data
+        var numHeroesNoWins = data.heroesNoWins.length;
+        var numHeroesWon = numHeroesTotal - numHeroesNoWins;
+        var heroesWonPercentage = Math.round((numHeroesWon / numHeroesTotal) * 100);
+        $("#heroesWonText").html(numHeroesWon + "/" + numHeroesTotal + " (" + heroesWonPercentage + "%)");
+        var heroesWonChartData = [
+            {value: numHeroesWon, color: "#268bd2"},
+            {value: numHeroesNoWins, color: "#93a1a1"}
+        ];
+        var ctx = $("#heroesWonChart").get(0).getContext("2d");
+        var heroesWonChart = new Chart(ctx).Doughnut(heroesWonChartData);
+
+        // Update heroes no wins 
+        $("#heroesNoWinsText").html(numHeroesNoWins);
+        if (numHeroesNoWins > 0) {
+            // Wipe table and repopulate with data.heroesNoWins
+            var heroesNoWinsTable = $("#heroesNoWinsTable");
+            heroesNoWinsTable.empty();
+            for (i = 0; i < data.heroesNoWins.length; i++) {
+                console.log("data.heroesNoWins[" + i + "]: " + data.heroesNoWins[i]);
+                var heroName = data.heroesNoWins[i].name;
+                var heroImage = data.heroesNoWins[i].image;
+                console.log("add " + heroName);
+                heroesNoWinsTable.append("<tr><td class=\"heroText\"><p>" + heroName + "</p><td class=\"heroImage\"><img src=\"" + heroImage + "\" /></td></tr>");
+            }
+        } else {
+            $("#heroesNoWins").fadeOut(3000);
+        }
+
+        // Show the tracker content
+        $("#introContent").hide();
+        $("#trackerContent").show();
+    } else {
+        $("#introContent").show();
+        $("#trackerContent").hide();
+    }
+}
+/*
+$(document).ready(function() {
     // Listen to changes to url input on navbar
     $("#urlInput").on('keyup', function(event) {
         if (event.keyCode === 13) {
@@ -90,3 +90,4 @@ $(document).ready(function() {
         }
     });
 });
+*/
